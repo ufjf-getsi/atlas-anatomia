aae34b9af63186126001e29487737571c6ac7710
João Pedro <joaoytestudos@gmail.com>
2023-07-25 15:27:27 -0300

    inicia o trabalho com os dados em formato json

--- _javascript/alternativo.js
+++ _javascript/alternativo.js
@@ -1,48 +1,102 @@
 
+let data, images, titles, pinsURL;
+let imageIndex = 0;
 
-// carregar a página quando a tela for carregada
-const onLoadWindow = () => {
+// busca os dados do JSON
+const fetchData = async (url) => {
+    const data = await fetch(url);
+    const json = await data.json();
 
-    // recebe o JSON por localStorage  
-    const data = JSON.parse(localStorage.data);
-    console.log("DATA: ", data);
+    return json;
+}
 
-    const mainTitle = data[0];
-    images = data[1];
-    titles = data[2];
-    const imagesNumber = images.length;
+// carrega os dados quando a tela for carregada
+const onLoadWindow = async (system) => {
+
+    // carrega o JSON
+    const url = "../_utils/sistemas.json";
+    let data = await fetchData(url)
     
-    // selecionando os elementos no html pra manipular
-    const container = document.getElementById("content");
-    const title = document.getElementById("title");
-    const navigations = document.getElementById("navigations");
+    const systemName = data.sistemas[system].name;
+    const systemURL = data.sistemas[system].src;
 
-    // carregando os selectores para navegar pelas imagens
+    // dados do sistema ( imagens, titulos...)
+    const systemData = await fetchData(systemURL)
+
+    images = systemData.images;
+    titles = systemData.titles;
+    pinsURL = systemData.alfinetes;
+    const imagesNumber = images.length;
+
+    loadPins(pinsURL[imageIndex]);
+    loadNavigations(imagesNumber)
+
+    // seleciona os elementos no html pra manipular
+    const backgroundImage = document.getElementById("content");
+    const imageTitle = document.getElementById("imageTitle");
+    const mainTitle = document.getElementById("mainTitle");
+    
+    backgroundImage.src = images[imageIndex];
+    imageTitle.innerText = titles[imageIndex];
+    mainTitle.innerHTML= "Sistema " + systemName;
+
+}
+
+// carrega as navegações paranavegar pelas imagens
+const loadNavigations = (imagesNumber) => {
+
     let links = "";
+    
     for(let i=0; i<imagesNumber; i++) {
         links += `<button class="selectors ${resolve(i)}" onClick=navigateToImage(${i}) id="item_${i}"></button>`
     }
 
+    const navigations = document.getElementById("navigations");
     navigations.innerHTML = links;
-    container.innerHTML = `<object data="${images[0]}" >`
-    title.innerText = mainTitle;
 }
 
-// função que marca o primeiro item como selecionado assim que inicia as imagens
+const loadPins = async (pinsURL) => {
+
+    const pinsData = await fetchData(pinsURL);
+    const { pins } = pinsData; 
+
+    const pinsArea = document.getElementById("pinsArea");
+    pinsArea.innerHTML= "";
+
+    let pinsHTML = "";
+
+    pins.forEach(pin => {
+        pinsHTML += `<div class="alfinete" style="position: absolute; left: ${pin.posX};top: ${pin.posY};">
+            ${resolvePinType(pin.type)}            
+        </div>`
+    });
+
+    pinsArea.innerHTML += pinsHTML;
+}
+
+// marca o primeiro item (i == 0) como selecionado assim que carrega as imagens
 const resolve = (i) => {
     if(!i)
         return "selected";
 }
 
+// retorna a imagem de acordo com tipo (true = vermelho ; false = azul)
+const resolvePinType = (type) => {
+    if(type) 
+        return `<img src="../_imagens/alfinete_vermelho.png" style="width: 50px" />`
+    else return `<img src="../_imagens/alfinete_azul.png" style="width: 50px" />`
+}
+
 // navega para a página/imagem de acordo com o index
 const navigateToImage = (index) => {
     
-    let fileName = images[index];
-	let title = titles[index];
-	let target = `<object id="loadedFile" data="${fileName}"/>'`;
+    // começa a carregar os alfinetes
+    loadPins(pinsURL[index])
 
-    const container = document.getElementById("content");
-    const imgTitle = document.getElementById("title");
+    const backgroundImage = document.getElementById("content");
+    const imageTitle = document.getElementById("imageTitle");
+
+    // seletores
     const willBeSelected = document.getElementById(`item_${index}`);
     const selected = document.getElementsByClassName("selected");   
 
@@ -50,11 +104,12 @@ const navigateToImage = (index) => {
     selected[0] ? selected[0].classList.remove("selected") : {};
     willBeSelected.classList.add("selected");
 
-    container.innerHTML = target;
-    imgTitle.innerText = title;
+    backgroundImage.src = images[index];
+    imageTitle.innerText = titles[index];
+
 }
 
-function slideRight(){
+const slideRight = () => {
 	imageIndex++;
 
 	if(imageIndex >= images.length){
@@ -64,7 +119,7 @@ function slideRight(){
 	navigateToImage(imageIndex);
 }
 
-function slideLeft(){
+const slideLeft = () => {
 	imageIndex--;
 
 	if(imageIndex < 0){
@@ -73,3 +128,15 @@ function slideLeft(){
 
 	navigateToImage(imageIndex);
 }
+
+const showCoordinates = (e) => {
+
+    const backgroundImage = document.getElementById("content");
+    const dimensions = backgroundImage.width;
+
+    let px = (e.offsetX -10)/dimensions * 100;
+    let py = (e.offsetY -50)/dimensions * 100;
+
+    console.log("PosX: ", px.toFixed(2), "PosY: ", py.toFixed(2))
+}
+
